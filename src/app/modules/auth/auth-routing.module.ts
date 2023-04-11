e714551c952f83380d2ab3881cdfa5762f3019da
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-04-11 17:36:51 -0500

    redirectTo login

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthPageComponent } from './page/auth-page/auth-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: AuthPageComponent},
+  {path: '**', redirectTo: '/auth/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
