91be662601089dc2091644d5d7c4a3900c10b6a6
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-04-11 13:29:15 -0500

    connect Array<TrackModel> with tracks.json

--- src/app/shared/components/play-list-header/play-list-header.component.ts
+++ src/app/shared/components/play-list-header/play-list-header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-play-list-header',
   templateUrl: './play-list-header.component.html',
   styleUrls: ['./play-list-header.component.css']
 })
-export class PlayListHeaderComponent {
+export class PlayListHeaderComponent implements OnInit{
+  constructor() {}
 
+  ngOnInit(): void {}
 }
