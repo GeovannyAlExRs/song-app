b8b13960da444e9d828c6825661b7527c61ba4ca
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-04-11 17:16:34 -0500

    add img default in case of error the loading image

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appImgBroken]'
+  selector: 'img[appImgBroken]'
 })
 export class ImgBrokenDirective {
 
-  constructor() { }
+  //@Input() customImg: string = ''
+
+  // Para cargar Imagen Default en caso de no encontrar la imagen
+  @HostListener('error') handleError(): void {
+    const elemNative = this.elemHost.nativeElement
+    console.log('Imagen no encontrada', this.elemHost)
+    elemNative.src = '../../../assets/image/songappx512.png'
+  }
+
+  constructor(private elemHost: ElementRef) {
+
+  }
 
 }
