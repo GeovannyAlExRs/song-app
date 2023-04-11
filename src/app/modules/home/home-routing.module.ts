7a35a5a91456ca3f6be2e361838161731318a3ca
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-04-10 21:54:14 -0500

    Routing Modules Home with Tracks

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from '@modules/home/page/home-page/home-page.component';
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent}
+  //{path: '', component: HomePageComponent}
+  {path: 'tracks', loadChildren: () => import('@modules/tracks/tracks.module').then(m => m.TracksModule)},
+  {path: 'favorites', loadChildren: () => import('@modules/favorites/favorites.module').then(m => m.FavoritesModule)},
+  {path: 'history', loadChildren: () => import('@modules/history/history.module').then(m => m.HistoryModule)}
 ];
 
 @NgModule({
