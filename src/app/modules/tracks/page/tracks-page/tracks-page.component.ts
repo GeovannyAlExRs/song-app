15f805fce90c3519037612c9cd698df47a49dd35
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-04-12 14:57:05 -0500

    Program RxJS Tracks through Observable-Subscribe

--- src/app/modules/tracks/page/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/page/tracks-page/tracks-page.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 
 import * as dataRaw from '../../../../data/tracks.json'
+import { TrackService } from '@modules/tracks/service/track.service';
 
 @Component({
   selector: 'app-tracks-page',
@@ -11,13 +12,19 @@ import * as dataRaw from '../../../../data/tracks.json'
 })
 export class TracksPageComponent implements OnInit{
 
-  mockTrackList:Array<TrackModel> =[]
+  tracksTrending:Array<TrackModel> =[]
+  tracksRandom:Array<TrackModel> =[]
 
-  constructor() {}
+  constructor(private _trackService:TrackService) {}
 
   ngOnInit(): void {
-    const { data }:any = (dataRaw as any).default
+    /*const { data }:any = (dataRaw as any).default
     this.mockTrackList = data
-    console.log(data)
+    console.log(data)*/
+    const observer1$ = this._trackService.dataTracksTrending$.subscribe(
+      (response) => {
+        console.log('Canciones Tracks Trending: ', response)
+      }
+    )
   }
 }
