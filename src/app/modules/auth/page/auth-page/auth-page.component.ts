a5e8418867f90a364d638101f51cb9bfcb9a55bd
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-04-12 01:06:43 -0500

    validator form react

--- src/app/modules/auth/page/auth-page/auth-page.component.ts
+++ src/app/modules/auth/page/auth-page/auth-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
@@ -6,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-page.component.css']
 })
 export class AuthPageComponent implements OnInit{
+
+  formLogin: FormGroup = new FormGroup({})
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formLogin = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(16) ])
+      }
+    )
+  }
+
+  sendLogin(): void{
+    const body = this.formLogin.value
+    console.log('DATA: ', body)
+  }
 }
