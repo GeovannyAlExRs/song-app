188964ba9584098ef168b7bfa86c6e366eddd8aa
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-04-14 14:41:39 -0500

    implement ngModel and ngModelChange

--- src/app/modules/history/components/search/search.component.ts
+++ src/app/modules/history/components/search/search.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
-  src: string = 'Buscar'
+export class SearchComponent implements OnInit {
 
+  src: string = ''
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  callSearch(valor: string): void {
+    if(valor.length >= 3) {
+      console.log('VALOR: ',valor);
+    }
+
+  }
 }
