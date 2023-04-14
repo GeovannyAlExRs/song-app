fcdabe9dab073ac8f68e1af2ffceda1b79556de6
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-04-14 10:59:14 -0500

    check token exist for redirecto login

--- src/app/core/guards/session.guard.ts
+++ src/app/core/guards/session.guard.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionGuard implements CanActivate {
+
+  constructor(private _cookieService: CookieService, private _router: Router) {
+
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.checkCookieSession();
   }
-  
+
+  checkCookieSession(): boolean {
+    try {
+
+      const token: boolean = this._cookieService.check('token')
+      console.log('OK OK OK ', token);
+
+      if(!token){
+        this._router.navigate(['/', 'auth'])
+      } else {
+        return token
+      }
+
+    } catch (error) {
+      console.log('Ups! Algo salio mal... ', error);
+      return false
+    }
+    return true
+  }
 }
