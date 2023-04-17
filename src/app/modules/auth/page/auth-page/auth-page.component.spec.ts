15403b36558d47f87627a6eb66b431f0afc73221
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-04-16 21:40:07 -0500

    test login form

--- src/app/modules/auth/page/auth-page/auth-page.component.spec.ts
+++ src/app/modules/auth/page/auth-page/auth-page.component.spec.ts
@@ -4,6 +4,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { AuthPageComponent } from './auth-page.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('AuthPageComponent', () => {
   let component: AuthPageComponent;
@@ -21,7 +22,35 @@ describe('AuthPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // PATRON AAA (Arrange Act Assert)
+  it('Retorno de formulario invalido', () => {
+    const mockCredentials = {email: 'abc.ec', password: '1234567891011121314151617181920'}
+
+    const emailForm: any = component.formLogin.get('email')
+    const passwordForm: any = component.formLogin.get('password')
+
+    emailForm.setValue(mockCredentials.email)
+    passwordForm.setValue(mockCredentials.password)
+
+    expect(component.formLogin.invalid).toEqual(true);
+  });
+
+  it('Retorno de formulario valido', () => {
+    const mockCredentials = {email: 'test@test.com', password: '12345678'}
+
+    const emailForm: any = component.formLogin.get('email')
+    const passwordForm: any = component.formLogin.get('password')
+
+    emailForm.setValue(mockCredentials.email)
+    passwordForm.setValue(mockCredentials.password)
+
+    expect(component.formLogin.invalid).toEqual(false);
+  });
+
+  it('Verificar boton de Iniciar Session', () => {
+    const elementRef = fixture.debugElement.query(By.css('.form-action button'))
+    const getInnerText = elementRef.nativeElement.innerText
+
+    expect(getInnerText).toEqual('Iniciar Sesión');
   });
 });
